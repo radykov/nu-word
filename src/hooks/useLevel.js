3274fca7c74828f2406d69367ec2effb003eb7e9
radykov <radykov>
2025-03-29 15:57:16 +0800

    removed unused hook

--- src/hooks/useLevel.js
+++
@@ -1,6 +0,0 @@
-import { useContext } from 'react';
-import { LevelContext } from './LevelContext'; // assuming LevelContext is exported
-
-export default function useLevel() {
-    return useContext(LevelContext);
-}
